f138837475b9cc569f59464d43853eb9c50cddd2
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-07-27 20:36:23 -0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const PRODUCTS = [
+    {
+        id: 1,
+        name: "Ração Premium Cães",
+        price: 120,
+        category: "dog",
+        image: "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e"
+    },
+    {
+        id: 2,
+        name: "Brinquedo Mordedor",
+        price: 35,
+        category: "dog",
+        image: "https://images.unsplash.com/photo-1601758123927-196a2f79c4b3"
+    },
+    {
+        id: 3,
+        name: "Ração Gatos",
+        price: 90,
+        category: "cat",
+        image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131"
+    },
+    {
+        id: 4,
+        name: "Arranhador Premium",
+        price: 150,
+        category: "cat",
+        image: "https://images.unsplash.com/photo-1595433707802-66f9ddcfb3f3"
+    }
+];
